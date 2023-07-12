d314362d408519b32592664ef9da96b77e4f6797
lucasstranger1 <132631837+lucasstranger1@users.noreply.github.com>
2023-07-12 18:13:45 -0400

    Update electives.php

--- electives.php
+++ electives.php
@@ -3,72 +3,124 @@
     <head>
         <title>Electives Courses</title>
         <style>
-        body {
-            /* background-color: rgba(0, 0, 0, 0.5); */
+            *,
+            *::before,
+            *::after{
+                box-sizing: border-box;
+            }
+            body {
             font-family: Arial, sans-serif;
-            margin: 0px;
-            padding: 0px;
-            text-align: center;
-            font-size: 15px;
-            color: black;
+            background-image: url("bmccbackground.jpeg");
         }
 
+     
+        .container {            
+            background-color: rgba(255, 255, 255, 0.9);
+            margin-top: 250px;
+            max-width: 1200px;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 20px;
+            background-color: #f9f9f9;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        }
+
+        .table-container {
+            margin-bottom: 30px; 
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            background-color: #fff;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        }
+
+        table caption {
+            font-weight: bold;
+            margin-bottom: 10px;
+            cursor: pointer;
+        }
+
+        table th,
+        table td {
+            padding: 10px;
+            text-align: left;
+        }
+
+        table th {
+            background-color: #f1f1f1;
+            font-weight: bold;
+        }
+
+        table tbody tr:nth-child(even) {
+            background-color: #f9f9f9;
+        }
+
+        table tbody tr:hover {
+            background-color: #f5f5f5;
+        }
+
+        .arrow-icon::before {
+            font-family: "Font Awesome 5 Free";
+            font-weight: 900;
+            content: "\f078";
+            margin-right: 5px;
+        }
+
+        .arrow-icon.collapsed::before {
+            content: "\f077";
+        }
+        .icon {
+            width: 150px;
+            height:40px;       
+        }
+        .Topbar {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                display: flex; 
+                justify-content: space-between;
+                background-color: #1c8fa9;
+                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.8);
+                z-index: 1000;
+                overflow: hidden;
+                height: 40px;               
+                }
+
+                .menu-items {
+            flex-grow: 1;
+            display: flex;
+            justify-content: flex-end;
+        }
+      
+
+        .Topbar li {
+            float: left;
+        }
+    
+        .Topbar li a {            
+            color: white;
+            text-align: left;
+            padding: 14px 8px 8px 8px;
+            text-decoration: none;
+            font-weight: bold;       
+            background-color: #1c8fa9;
+            align-items: center;
+        }      
+        .Topbar a:hover {
+            color: #ddd;
+        }
         .header {
             background-color: #f1f1f1;
             text-align: center;
         }
-
-        .container {
-            margin-top:100px;
-            background-image: url("bmccbackground.jpeg");
-            background-repeat: no-repeat;
-            background-size: contain;
-            background-position: center center;
-            background-attachment: fixed;
-            width: 100%;
-            height: 100%;
-           
-        }
-
-        tr:hover {
-            background-color: #ddd;
-        }
-        .table-name {
-            text-align: left;
-            font-weight: bold;
-            font-size: 30px;
-        }
-        table {
-            margin: auto;
-            border-collapse: collapse;
-            width: 80%;
-        }
-
-        th,
-        td {
-            padding: 10px;
-            border: 1px solid #ddd;
-        }
-        th {
-            background-color: #f1f1f1;
-            font-weight: bold;
-        }
-        .content {
-            background-color: rgba(255, 255, 255, 0.9);
-            max-width: 100%;
-            margin: auto;
-            padding-bottom:45px;
-
-        }
-
-        .icon {
-            width: 150px;
-            height:21px;
-            margin-right: 10px;
-        }
-
- 
-
+        
+       
         .search-bar {
             float: left;
             margin-top: 8px;
@@ -115,44 +167,6 @@
             font-weight: bold;
         }
 
-        .menu-items {
-            flex-grow: 1;
-            display: flex;
-            justify-content: flex-end;
-        }
-        .Topbar {
-                position: fixed;
-                top: 0;
-                left: 0;
-                width: 100%;
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                background-color: #1c8fa9;
-                box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.8);
-                z-index: 1000;
-                }
-
-        .Topbar li {
-            float: left;
-        }
-    
-        .Topbar li a {
-            /* display: inline-block; */
-            color: white;
-            text-align: left;
-            padding: 14px 8px 8px 8px;
-            text-decoration: none;
-            font-weight: bold;       
-            background-color: #1c8fa9;
-            
-        }      
-        .Topbar a:hover {
-            color: #ddd;
-        }
         .fa-solid {
                 font-size: 20px; /* Adjust the size as needed */
                 margin-right: 10px; /* Optional: Adds some spacing between the icon and the text */
@@ -209,88 +223,107 @@
     </header>
 
     <div class="container">
-
         <ul class="h1"><i class="fa-solid fa-list"></i>Electives Courses List
             <li class="note">
-                Note: The table is the list of electives courses that shows four categories:Individual and Society, US Experience in Its Diversity, World Cultures and Global Issues, and Program Electives  
+                Note: The table is the list of elective courses that shows four categories: Individual and Society, US Experience in Its Diversity, World Cultures and Global Issues, and Program Electives.
             </li>
         </ul>
-        <div class="content">
-        <table>
-            <thead>
-                <tr>
-                    <th>Table Name</th>
-                    <th>Course Name</th>
-                    <th>Credit</th>
-                    <th>Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                // Establish a connection to the database
-                    $majorId = $_POST['major'];
-                    $servername = "sql212.infinityfree.com";
-                    $username = "if0_34373034";
-                    $password = "g9LDz1n2hE";  
-                    $dbname = "if0_34373034_db_electives_courses";
+       
+        <?php
+            // Establish a connection to the database
+            $servername = "127.0.0.1";
+            $username = "root";
+            $password = "";
+            $dbname = "Electives_courses";
 
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            // SQL query to retrieve the course information
+            $query = "SELECT id, course_name, credit, description, 'Individual and Society' AS table_name FROM `Individual and Society`
+                      UNION
+                      SELECT id, course_name, credit, description, 'US Experience in Its Diversity' AS table_name FROM `US Experience in Its Diversity`
+                      UNION
+                      SELECT id, course_name, credit, description, 'World Cultures and Global Issues' AS table_name FROM `World Cultures and Global Issues`
+                      UNION
+                      SELECT id, course_name, credit, description, 'Program Electives' AS table_name FROM `Program Electives`";
+
+            $result = $conn->query($query);
+
+            if ($result->num_rows > 0) {
+                $tableNames = [];
+
+                while ($row = $result->fetch_assoc()) {
+                    $tableName = $row['table_name'];
+
+                    if (!in_array($tableName, $tableNames)) {
+                        $tableNames[] = $tableName;
+                    }
                 }
 
-                // SQL query to retrieve the course information
-           $query = "SELECT id, course_name, credit, description, 'Individual and Society' AS table_name FROM `Individual and Society`
-          UNION
-          SELECT id, course_name, credit, description, 'US Experience in Its Diversity' AS table_name FROM `US Experience in Its Diversity`
-          UNION
-          SELECT id, course_name, credit, description, 'World Cultures and Global Issues' AS table_name FROM `World Cultures and Global Issues`
-          UNION
-          SELECT id, course_name, credit, description, 'Program Electives' AS table_name FROM `Program Electives`";
+                foreach ($tableNames as $tableName) {
+                    echo "<table>";
+                        echo "<caption onclick='toggleCourseList(\"$tableName\")'>";
+                        echo "<span class='arrow-icon' id='arrow-icon-$tableName'></span>";
+                        echo "$tableName";
+                        echo "</caption>";
+                        echo "<thead>";
+                        echo "<tr>";
+                        echo "<th>Course Name</th>";
+                        echo "<th style='width: 80px;'>Credit</th>";
+                        echo "<th style='width: 200px;'>Description</th>";
+                        echo "</tr>";
+                        echo "</thead>";
+                        echo "<tbody id='course-list-$tableName' style='display: none;'>";
 
-                $result = $conn->query($query);
-
-                if ($result->num_rows > 0) {
-                    $tableNames = [];
+                    // Fetch and display the course list for the current category
+                    $result->data_seek(0); // Reset the result pointer
 
                     while ($row = $result->fetch_assoc()) {
-                        $tableName = $row['table_name'];
-
-                        if (!in_array($tableName, $tableNames)) {
-                            $tableNames[] = $tableName;
+                        if ($row['table_name'] === $tableName) {
+                            echo "<tr>";
+                            echo "<td>" . $row['course_name'] . "</td>";
+                            echo "<td>" . $row['credit'] . "</td>";
+                            echo "<td>" . $row['description'] . "</td>";
+                            echo "</tr>";
                         }
                     }
 
-                    foreach ($tableNames as $tableName) {
-                        echo "<tr><td colspan='4' class='table-name'>$tableName</td></tr>";
-
-                        $result->data_seek(0); // Reset the result pointer
-
-                        while ($row = $result->fetch_assoc()) {
-                            if ($row['table_name'] === $tableName) {
-                                echo "<tr>";
-                                echo "<td></td>"; // Empty cell for the table name
-                                echo "<td>" . $row['course_name'] . "</td>";
-                                echo "<td>" . $row['credit'] . "</td>";
-                                echo "<td>" . $row['description'] . "</td>";
-                                echo "</tr>";
-                            }
-                        }
-                    }
-                } else {
-                    echo "<tr><td colspan='4'>No data available</td></tr>";
+                    echo "</tbody>";
+                    echo "</table>";
                 }
+            } else {
+                echo "<p>No data available</p>";
+            }
 
-                $conn->close();
-                ?>
-            </tbody>
-        </table>
-    </div>
- 
-    <footer class="footer">
-        @ Borough of Manhattan Community College | 199 Chambers Street | New York, NY 10007 | Contact Us | Privacy Policy
-    </footer>
-    </div>
+            $conn->close();
+            ?>
 
-    </body>
-    </html>
+    </div>
+     
+        <footer class="footer">
+            @ Borough of Manhattan Community College | 199 Chambers Street | New York, NY 10007 | Contact Us | Privacy Policy
+        </footer>
+        </div>
+        <script>
+        // Function to toggle visibility of course lists
+        function toggleCourseList(categoryId) {
+            const courseList = document.getElementById(`course-list-${categoryId}`);
+            const arrowIcon = document.getElementById(`arrow-icon-${categoryId}`);
+
+            if (courseList.style.display === 'none') {
+                courseList.style.display = 'block';
+                arrowIcon.classList.remove('fa-chevron-down');
+                arrowIcon.classList.add('fa-chevron-up');
+            } else {
+                courseList.style.display = 'none';
+                arrowIcon.classList.remove('fa-chevron-up');
+                arrowIcon.classList.add('fa-chevron-down');
+            }
+        }
+    </script>
+        </body>
+        </html>
+    
