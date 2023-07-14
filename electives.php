173bbc19b50c43abad6ee32106096ec75d1d1d79
lucasstranger1 <132631837+lucasstranger1@users.noreply.github.com>
2023-07-14 19:36:17 -0400

    Update electives.php

--- electives.php
+++ electives.php
@@ -16,7 +16,7 @@
      
         .container {            
             background-color: rgba(255, 255, 255, 0.9);
-            margin-top: 250px;
+            margin-top: auto;
             max-width: 1200px;
             margin-left: auto;
             margin-right: auto;
@@ -188,11 +188,43 @@
             z-index: 1;
 
         }
+        .container1 {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: whitesmoke;
+        }
+        .search-container {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+
+        .search-container input[type="text"] {
+            padding: 8px;
+            font-size: 16px;
+            width: 300px;
+        }
+
+        .search-container button {
+            padding: 8px 16px;
+            margin-top: 10px;
+            font-size: 16px;
+            background-color: #4CAF50;
+            color: #fff;
+            border: none;
+            cursor: pointer;
+            border-radius: 10px;
+        }
 
     </style>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
-        <script src="https://kit.fontawesome.com/0d370ca485.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js"></script>
+    <script src="https://kit.fontawesome.com/0d370ca485.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
     </head>
     <body>
     <header>
@@ -221,7 +253,98 @@
             </ul>
         </nav>
     </header>
+    <div class="container1">
+        <h1>Search Elective Course</h1>
+        <div class="search-container">
+            <form method="POST" action="">
+            <input type="text" name="search" id="search-input" placeholder="Enter your Elective course name" autocomplete="off">
+                <div id="search-results-container"></div>
+             
 
+                <button type="submit" name="submit">Search</button>
+            </form>
+        </div>
+
+        <?php
+     /* Database credentials*/
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'Electives_courses');
+ 
+/* Attempt to connect to MySQL database */
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+        $searchResults = [];
+
+        if (isset($_POST['submit'])) {
+            // Get the search query
+            $search = $_POST['search'];
+
+            // Check if the search query is not empty
+            if (!empty($search)) {
+                // Perform the search operation and store the results
+                $query = "SELECT id, course_name, credit, description, 'Individual and Society' AS table_name FROM `Individual and Society`
+                WHERE course_name LIKE '%" . $search . "%'
+                UNION
+                SELECT id, course_name, credit, description, 'US Experience in Its Diversity' AS table_name FROM `US Experience in Its Diversity`
+                WHERE course_name LIKE '%" . $search . "%'
+                UNION
+                SELECT id, course_name, credit, description, 'World Cultures and Global Issues' AS table_name FROM `World Cultures and Global Issues`
+                WHERE course_name LIKE '%" . $search . "%'
+                UNION
+                SELECT id, course_name, credit, description, 'Program Electives' AS table_name FROM `Program Electives`
+                WHERE course_name LIKE '%" . $search . "%'";
+
+                $result = $conn->query($query);
+
+                // Store the search results
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $searchResults[] = [
+                            'table_name' => $row['table_name'],
+                            'course_name' => $row['course_name'],
+                            'credit' => $row['credit'],
+                            'description' => $row['description']
+                        ];
+                    }
+                }
+            }
+        }
+        ?>
+
+        <?php if (!empty($searchResults)): ?>
+            <?php foreach ($searchResults as $result): ?>
+                <table>
+                    <caption>
+                        <?php echo $result['table_name']; ?>
+                    </caption>
+                    <thead>
+                        <tr>
+                            <th class="course-name">Course Name</th>
+                            <th class="credit">Credit</th>
+                            <th class="description">Description</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $result['course_name']; ?></td>
+                            <td><?php echo $result['credit']; ?></td>
+                            <td><?php echo $result['description']; ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <?php if (isset($_POST['submit'])): ?>
+                <p>No data available</p>
+            <?php endif; ?>
+        <?php endif; ?>
+    </div>
     <div class="container">
         <ul class="h1"><i class="fa-solid fa-list"></i>Electives Courses List
             <li class="note">
@@ -231,16 +354,12 @@
        
         <?php
             // Establish a connection to the database
-            $servername = "127.0.0.1";
-            $username = "root";
-            $password = "";
-            $dbname = "Electives_courses";
-
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
+ 
+ 
+// Check connection
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
             // SQL query to retrieve the course information
             $query = "SELECT id, course_name, credit, description, 'Individual and Society' AS table_name FROM `Individual and Society`
                       UNION
@@ -324,6 +443,100 @@
             }
         }
     </script>
+        <script>
+        // Function to toggle visibility of course lists
+        function toggleCourseList(categoryId) {
+            const courseList = document.getElementById(`course-list-${categoryId}`);
+            const arrowIcon = document.getElementById(`arrow-icon-${categoryId}`);
+
+            if (courseList.style.display === 'none') {
+                courseList.style.display = 'block';
+                arrowIcon.classList.remove('fa-chevron-down');
+                arrowIcon.classList.add('fa-chevron-up');
+            } else {
+                courseList.style.display = 'none';
+                arrowIcon.classList.remove('fa-chevron-up');
+                arrowIcon.classList.add('fa-chevron-down');
+            }
+        }
+    </script>
+    <script>
+        // Function to check if the search query is present
+        function isSearchQueryPresent() {
+            const searchInput = document.querySelector('input[name="search"]');
+            return searchInput.value.trim() !== '';
+        }
+
+        // Check if the search query is present, and if not, hide the search results section
+        window.addEventListener('load', () => {
+            const searchResults = document.querySelector('.search-results');
+
+            if (!isSearchQueryPresent()) {
+                searchResults.style.display = 'none';
+            } else {
+                searchResults.style.display = 'block';
+            }
+        });
+    </script>
+<script>
+$(document).ready(function() {
+  const courseNames = ['ACL 200 - Literacy Practices: Birth through Adolescence',
+'COM 100 - Introduction to Communication Studies',
+'COM 240 - Interpersonal Communication',
+'CRT 100 - Critical Thinking',
+'CRT 120 - Critical Thinking and Social Justice',
+'CRT 196 - Critical Thinking: Inquiry through Queer Theories',
+'CRT 245 - Critical Thinking and Media Literacy',
+'ECO 100 - Introduction to Economics',
+'ECO 202 - Microeconomics',
+'GWS 100 - Introduction to Gender and Women\'s Studies',
+'HED 190 - Human Sexuality and Society',
+'HIS 101 - Western Civilization: From Ancient to Early Modern Times',
+'HIS 102 - Western Civilization: The Emergence of the Modern World',
+'LIN 100 - Language and Culture',
+'MES 152 - Introduction to Contemporary Media',
+'PHI 100 - Philosophy',
+'PHI 120 - Ethics',
+'POL 110 - Introduction to Politics',
+'POL 120 - Gender and Politics',
+'SOC 100 - Introduction to Sociology',
+'SOC 111 - Understanding Technological Society',
+'ACR 150 - Literacy in American Society',
+'AFL 100 - Introduction to Ethnic Studies',
+'AFL 125 - Comparative Ethnic Studies',
+'AFL 161 - Health Problems in Urban Communities',
+'AFN 123 - African-American History, 17th Century to 1865',
+'AFN 124 - African-American History, 1865 to Present',
+'AFN 129 - The Black Man in Contemporary Society',
+'ASN 114 - Asian American History',
+'ASN 211 - Asian Americans in NYC',
+'ECO 201 - Macroeconomics',
+'HED 195 - Food, Culture and Society',
+'HIS 120 - Early American History: Colonial Period to Civil War',
+'HIS 125 - Modern American History: Civil War to Present',
+'LAT 125 - Puerto Rican Culture and Folklore',
+'LAT 140 - Introduction to Mexican-American Studies',
+'LAT 150 - The Latino Experience in the U.S.',
+'LIN 150 - Language, Race, and Ethnicity in the US and its Territories',
+'POL 100 - American Government',
+'SPN 485 - New York Literature in Spanish'
+]; // Replace with your own list of course names
+
+  $('#search-input').autocomplete({
+    source: courseNames,
+    minLength: 2,
+    select: function(event, ui) {
+      // Handle the selected suggestion
+      $('#search-input').val(ui.item.value);
+      // Perform the search or other actions here
+      // ...
+      return false;
+    }
+  });
+});
+
+
+</script>
         </body>
         </html>
     
